b14b6f3ac172af95d8156a02e19fee8705626640
Viktoriia Honcharova <vika.honcharova.619853@gmail.com>
2022-10-06 11:22:22 +0300

    Added form

---
+++ src/Form.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+export default function Form() {
+  return (
+    <div className="Form">
+      <form id="search-form">
+        <input
+          type="search"
+          placeholder="Enter a city"
+          autoFocus="off"
+          autoComplete="off"
+          className="input"
+          id="input"
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="search-button"
+          id="search-button"
+        />
+        <button className="current-location" id="current-location">
+          {" "}
+          <span>📍</span>
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "./styles.css";
+import Form from "./Form";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="container main">
+      <div className="row">
+        <div className="main-content col-sm-8">
+          <div className="content-top">
+            <Form />
+          </div>
+          <div className="content-bottom">
+            <div className="row bottom-row"></div>
+          </div>
+        </div>
+        <div className="prediction col-sm-4" id="forecast"></div>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
